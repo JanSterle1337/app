a450e325548562db1d7cecafd921612ee07b85a7
JanSterle1337 <jan.sterle123@gmail.com>
2022-09-08 10:20:12 +0200

    design finished #1

---
+++ tests/CheckCombinationFormatTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CheckCombinationFormatTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $inputedCombination = "1, 20, 30, 32, 34";
+        $repetition = 5;
+        $this->assertTrue(true);
+    }
+}
